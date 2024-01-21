84cfcf30c9aeb8380ce1344994def76c887b9c04
mlacker <mlcker@163.com>
2024-01-21 22:15:34 +0800

    Plugin GTFO

---
+++ BoosterRaisePlugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+namespace BoosterRaise;
+
+[BepInPlugin(GUID, NAME, VERSION)]
+public class BoosterRaisePlugin : BasePlugin
+{
+    const string GUID = "com.mlacker.plugins.BoosterRaise";
+    const string NAME = "BoosterRaise";
+    const string VERSION = "0.1.0";
+    
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {GUID} is loaded!");
+    }
+}
