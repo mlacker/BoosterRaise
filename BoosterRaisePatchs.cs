398e9d8ae322bcc3b5e9c3d15ba460c6231a7a0d
mlacker <mlcker@163.com>
2024-01-30 01:58:39 +0800

    feat: disable booster consume

--- BoosterRaisePatchs.cs
+++ BoosterRaisePatchs.cs
@@ -1,12 +1,28 @@
+using BoosterRaise.Common.Logging;
 using HarmonyLib;
+using Microsoft.Extensions.Logging;
 
 namespace BoosterRaise;
 
-public static class BoosterRaisePatchs {
+public static class BoosterRaisePatchs
+{
+    private static readonly ILogger logger = LoggerFactory.CreateLogger(nameof(BoosterRaisePatchs));
 
     [HarmonyPatch(typeof(DropServerManager), nameof(DropServerManager.NewGameSession))]
     [HarmonyPrefix]
     public static void NewGameSession(DropServerManager __instance, string sessionId, ref uint[]? boosterIds)
     {
+        logger.LogDebug($"Patch NewGameSession, sessionId: {sessionId}, boosters: [{boosterIds.Join()}]");
+
+        boosterIds = null;
+    }
+
+    [HarmonyPatch(typeof(DropServerGameSession), nameof(DropServerGameSession.ConsumeBoosters))]
+    [HarmonyPrefix]
+    public static bool ConsumeBoosters()
+    {
+        logger.LogDebug("Patch ConsumeBoosters, skip execute");
+
+        return false;
     }
 }
